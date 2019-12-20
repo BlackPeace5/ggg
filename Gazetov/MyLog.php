337c6458f130698145d28d20020f36fe939555a6
BlackPeace5 <57791853+BlackPeace5@users.noreply.github.com>
2019-12-21 02:59:53 +0300

    5.0

--- Gazetov/MyLog.php
+++ Gazetov/MyLog.php
@@ -15,6 +15,10 @@ Class MyLog extends LogAbstract implements LogInterface {
 	}
 	
 	public function _write() {
-		echo implode("\n", MyLog::Instance()->log);
+	
+		$d = new \DateTime();
+		$version = file_get_contents(__DIR__. "/../version");
+		file_put_contents(BASEURI . '/log/' . $d->format('d-m-Y\TH.i.s.u') . '.log', "File Version: " . $version . implode("\r\n", MyLog::Instance()->log));
+		echo implode("\r\n", MyLog::Instance()->log);
 	}
 }
